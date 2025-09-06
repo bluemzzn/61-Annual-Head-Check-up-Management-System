a392dbe35294971640127b47ff302e2a993724b7
bluemzzn <synthiawithsyren@gmail.com>
2025-09-06 09:31:55 +0700

    fopen function

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(){
+
+    FILE *file = fopen("Checkup-Data.csv", "r");
+
+    //check file opened
+    if(file == NULL){
+        printf("File: %p\n", file);
+        printf("cannot open the file");
+        
+        return 1;
+    }
+
+    int fclose(FILE *stream);
+
     return 0;
 }
