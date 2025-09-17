ec60f47a41dbf9a2acdd43c820adba6e7569dd81
bluemzzn <synthiawithsyren@gmail.com>
2025-09-17 16:47:01 +0700

    update displaymenu function

--- main.c
+++ main.c
@@ -1,24 +1,64 @@
 #include <stdio.h>
 
-// void addData()
-// {
-// }
-
-// void searchData()
-// {
-//     // from name and health status
-// }
-
-// void updateData()
-// {
-// }
-
-// void deleteData()
-// {
-// }
-
-int main()
+void addData()
 {
+    char fullname[30];
+    scanf("%s", fullname);
+    
+
+}
+
+void searchData()
+{
+    // from name and health status
+}
+
+void updateData()
+{
+
+}
+
+void deleteData()
+{
+
+}
+
+void exitMenu()
+{
+    printf("Thank you for using the program.\n");
+}
+
+void displayMenu()
+{
+    char alphabet;
+    scanf("%c", &alphabet);
+    printf("Menu Diaplay\n a.Add Data\n b.Search Data\n c.Update Data\n d.Delete data\n e.Exit Menu\n ");
+    printf("Please Enter an alphabet[a-e] to display a menu : %c");
+
+    switch (alphabet)
+    {
+    case 'a':
+        addData();
+        break;
+    case 'b':
+        searchData();
+        break;
+    case 'c':
+        updateData();
+        break;
+
+    case 'd':
+        deleteData();
+        break;
+
+    default:
+        exitMenu();
+        break;
+    }
+}
+
+int main(){
+    displayMenu();
 
     FILE *data = fopen("Checkup-Data.csv", "r");
 
